cfa3456bedce87ffddf57305625b97c6a28161c5
g0dd4rd <dr3dwerkz@gmail.com>
2023-02-19 12:51:06 +0100

    UPD: class with main(), ionian(String key) and array of notes to choose from

---
+++ Orbita.java
@@ -0,0 +1,12 @@
+public class Orbita {
+
+  String[] notes = {"C", "D", "E", "F", "G", "A", "B"};
+
+  void ionian(String key) {
+    int[] ionian = {0, 2, 2, 1, 2, 2, 2};
+  }
+
+  public static void main(String... args) {
+  }
+}
+
