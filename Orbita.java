0715eaafc32bb72dffda0c52bb46ada36aed611d
g0dd4rd <dr3dwerkz@gmail.com>
2023-02-19 13:19:28 +0100

    UPD: added chromatic notes and enharmonics and printing them out in main()

--- Orbita.java
+++ Orbita.java
@@ -1,12 +1,16 @@
 public class Orbita {
 
-  String[] notes = {"C", "D", "E", "F", "G", "A", "B"};
+  String[] notes = {"B# / C, ", "C# / Db, ", "D, ", "D# / Eb, ",  "E, ", "E# / F, ", "F# / Gb, ", "G, ", "G# / Ab, ", "A, ", "A# / Bb, ", "B, "};
 
   void ionian(String key) {
     int[] ionian = {0, 2, 2, 1, 2, 2, 2};
   }
 
   public static void main(String... args) {
+    Orbita orb = new Orbita();
+    for(var note : orb.notes) {
+      System.out.print(note);
+    }
   }
 }
 
